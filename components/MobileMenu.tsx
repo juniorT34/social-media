664b25b82d1e8e68bdd7885df997f62b10925da5
juniorT34 <jj1982268@gmail.com>
2024-07-16 00:47:36 +0300

    making the navbar responsive and interactive

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useState } from 'react'
 import { RxHamburgerMenu } from "react-icons/rx";
+import { IoMdClose } from "react-icons/io";
 import Link from "next/link"
 import { usePathname } from 'next/navigation';
 import { links } from '../constants';
@@ -10,11 +11,12 @@ const MobileMenu = () => {
     const pathname = usePathname()
     console.log("current path", pathname)
   return (
-    <div>
-        <RxHamburgerMenu className='w-6 h-6 text-blue-500 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
+    <div className='md:hidden'>
+        <RxHamburgerMenu className={`w-6 h-6 text-blue-500 cursor-pointer hover:text-blue-400 ${isOpen ? "hidden":""}`} onClick={() => setIsOpen(!isOpen)} />
+        <IoMdClose className={`w-6 h-6 text-blue-500 cursor-pointer hover:text-blue-400 ${!isOpen ? "hidden":""}`} onClick={() => setIsOpen(!isOpen)} />
             {
                 isOpen && (
-                    <div className='absolute top-12 left-0 w-full h-[calc(100vh-96px)] bg-white z-50 flex flex-col justify-center items-center gap-8 font-medium text-xl'>
+                    <div className='absolute top-14 left-0 w-full h-[calc(100vh-96px)] bg-white z-50 flex flex-col justify-center items-center gap-8 font-medium text-xl'>
                         {links.map((link,index) =>(
                             <Link key={index} href={link.href} className={pathname === link.href ? "text-blue-500" : "text-gray-500"}>{link.name}</Link>
                         ))}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,19 +1,36 @@
 import Link from "next/link"
 import MobileMenu from "./MobileMenu"
+import Image from "next/image"
 
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between w-full h-12">
         {/* LEFT */}
-        <div>
+        <div className="md:hidden lg:block w-[20%]">
             <Link href="/" className="font-bold text-xl text-blue-500">SocioLiepu</Link>
         </div>
-        {/* CENTER */}
-        <div className='hidden'>
 
+        {/* CENTER */}
+        <div className='hidden md:flex w-[50%] text-sm'>
+          {/* links */}
+          <div className='flex items-center gap-6 text-gray-600'>
+            <Link href="/" className="flex items-center gap-2">
+				<Image src={"/home.png"} width={16} height={16} alt="Homepage" />
+				<span>Homepage</span>
+            </Link>
+            <Link href="/friends" className="flex items-center gap-2">
+				<Image src={"/friends.png"} width={16} height={16} alt="Homepage" />
+				<span>Friends</span>
+            </Link>
+            <Link href="/stories" className="flex items-center gap-2">
+				<Image src={"/stories.png"} width={16} height={16} alt="Homepage" />
+				<span>Stories</span>
+            </Link>
+          </div>
         </div>
+
         {/* RIGHT */}
-        <div>
+        <div className="w-[30%] flex items-center justify-end gap-4 xl:gap-8">
             <MobileMenu />
         </div>
     </div>
